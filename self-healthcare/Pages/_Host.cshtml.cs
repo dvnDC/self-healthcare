726c0a94ad448eb29e5d7a28040ccb50f345b3d5
dvnDC <damcichosz@gmail.com>
2022-01-18 03:33:45 +0100

    Initial commit

---
+++ self-healthcare/Pages/_Host.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace self_healthcare.Pages
+{
+    public class _Host : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
