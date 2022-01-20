c053bd46d04666e62f7fcb5a3462ed721a967e89
dvnDC <damcichosz@gmail.com>
2022-01-20 04:27:34 +0100

    Add search option in Index

--- self-healthcare/Controllers/MoviesController.cs
+++ self-healthcare/Controllers/MoviesController.cs
@@ -20,9 +20,17 @@ namespace self_healthcare.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            return View(await _context.Movie.ToListAsync());
+            var movies = from m in _context.Movie
+                select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.Contains(searchString));
+            }
+
+            return View(await movies.ToListAsync());
         }
 
         // GET: Movies/Details/5
@@ -72,7 +80,7 @@ namespace self_healthcare.Controllers
             {
                 return NotFound();
             }
-
+        
             var movie = await _context.Movie.FindAsync(id);
             if (movie == null)
             {
