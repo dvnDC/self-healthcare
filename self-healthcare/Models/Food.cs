9a183a6e9c987ae14b1535cbecb1bdc3ec5eda38
dvnDC <damcichosz@gmail.com>
2022-01-25 23:14:45 +0100

    Add Food model

--- self-healthcare/Models/Food.cs
+++ self-healthcare/Models/Food.cs
@@ -2,9 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Globalization;
-using CsvHelper;
 using System.Linq;
-using CsvHelper.Configuration;
 
 namespace self_healthcare.Models;
 
