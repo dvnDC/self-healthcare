2da2b27a4cbbec8263dd5b1b0c256235e7c56ff0
dvnDC <damcichosz@gmail.com>
2022-01-26 01:57:22 +0100

    Add migration for Food table

---
+++ self-healthcare/Migrations/20220126004406_FoodTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace new_self_healthcare.Migrations
+{
+    public partial class FoodTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Food",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ServingSizeGrams = table.Column<int>(type: "INTEGER", nullable: false),
+                    Calories = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Food", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Food");
+
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
