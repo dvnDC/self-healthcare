cd8494b05ae77602be887cd6e01d827c645afefe
dvnDC <damcichosz@gmail.com>
2022-01-27 20:42:41 +0100

    Updated App User model

--- self-healthcare/Areas/Identity/Data/WebApp1User.cs
+++ self-healthcare/Areas/Identity/Data/WebApp1User.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using self_healthcare.Models;
 
 namespace self_healthcare.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the WebApp1User class
 public class WebApp1User : IdentityUser
 {
+    
+    [Required]
+    public virtual ICollection<Diet> Diet { get; set; }
 }
 
