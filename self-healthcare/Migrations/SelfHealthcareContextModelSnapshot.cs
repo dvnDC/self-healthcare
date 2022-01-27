6ba7e7c5f4e58e2ab26b90b58e353e300a9e5ce3
dvnDC <damcichosz@gmail.com>
2022-01-27 20:41:32 +0100

    Update system files

--- self-healthcare/Migrations/SelfHealthcareContextModelSnapshot.cs
+++ self-healthcare/Migrations/SelfHealthcareContextModelSnapshot.cs
@@ -16,6 +16,88 @@ namespace new_self_healthcare.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.1");
 
+            modelBuilder.Entity("self_healthcare.Areas.Identity.Data.WebApp1User", b =>
+                {
+                    b.Property<string>("Id")
+                        .HasColumnType("TEXT");
+
+                    b.Property<int>("AccessFailedCount")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("ConcurrencyStamp")
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Email")
+                        .HasColumnType("TEXT");
+
+                    b.Property<bool>("EmailConfirmed")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<bool>("LockoutEnabled")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<DateTimeOffset?>("LockoutEnd")
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("NormalizedEmail")
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("NormalizedUserName")
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("PasswordHash")
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("PhoneNumber")
+                        .HasColumnType("TEXT");
+
+                    b.Property<bool>("PhoneNumberConfirmed")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("SecurityStamp")
+                        .HasColumnType("TEXT");
+
+                    b.Property<bool>("TwoFactorEnabled")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("UserName")
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("WebApp1User");
+                });
+
+            modelBuilder.Entity("self_healthcare.Models.Diet", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("Calories")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<DateTime>("Inserted")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<int>("ServingSizeGrams")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("UserForeignKey")
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("UserForeignKey");
+
+                    b.ToTable("Diet");
+                });
+
             modelBuilder.Entity("self_healthcare.Models.Food", b =>
                 {
                     b.Property<int>("Id")
@@ -27,6 +109,7 @@ namespace new_self_healthcare.Migrations
 
                     b.Property<string>("Name")
                         .IsRequired()
+                        .HasMaxLength(50)
                         .HasColumnType("TEXT");
 
                     b.Property<int>("ServingSizeGrams")
@@ -60,6 +143,20 @@ namespace new_self_healthcare.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Movie");
+                });
+
+            modelBuilder.Entity("self_healthcare.Models.Diet", b =>
+                {
+                    b.HasOne("self_healthcare.Areas.Identity.Data.WebApp1User", "WebApp1User")
+                        .WithMany("Diet")
+                        .HasForeignKey("UserForeignKey");
+
+                    b.Navigation("WebApp1User");
+                });
+
+            modelBuilder.Entity("self_healthcare.Areas.Identity.Data.WebApp1User", b =>
+                {
+                    b.Navigation("Diet");
                 });
 #pragma warning restore 612, 618
         }
