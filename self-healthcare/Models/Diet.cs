5c449ab3f23a882db6ab9eacd6a3b0811cdf1b0a
dvnDC <damcichosz@gmail.com>
2022-01-27 20:40:20 +0100

    Create Diet model

--- self-healthcare/Models/Diet.cs
+++ self-healthcare/Models/Diet.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using self_healthcare.Areas.Identity.Data;
 
@@ -6,10 +7,15 @@ namespace self_healthcare.Models;
 public class Diet
 {
     
-    [ForeignKey("FoodForeignKey")]
-    public Food Food { get; set; }
-    
-    
+    [Key]
+    public int Id { get; set; }
     [ForeignKey("UserForeignKey")]
     public WebApp1User WebApp1User { get; set; }
+    
+    public string Name { get; set; }
+    public int ServingSizeGrams { get; set; }
+    public int Calories { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime Inserted { get; set; }
+    
 }
