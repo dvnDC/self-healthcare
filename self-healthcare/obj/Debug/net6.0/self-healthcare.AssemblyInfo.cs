f833456f55f68dfe2bbe572c06604ed17d3b2fdf
Damian Cichosz <damcichosz@gmail.com>
2024-03-08 22:55:44 +0100

    Update appsetings and csproj

--- self-healthcare/obj/Debug/net6.0/self-healthcare.AssemblyInfo.cs
+++ self-healthcare/obj/Debug/net6.0/self-healthcare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("self-healthcare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a2efeb6dad12378e69ee02e85197d62002766cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("self-healthcare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("self-healthcare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
